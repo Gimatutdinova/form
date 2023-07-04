2aa41c61ee228edd9157f15c40056e5aa6dff409
G Kamila <gimatutdinova.k@gmail.com>
2023-07-04 10:45:13 +0300

    Изменение функций

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
-const xhr = new XMLHttpRequest();
+let val = {}; // Объект для сохранение данных формы
+let str; // Переменная для сохранения данных формы в формате JSON
 
-$('#button').on('click', () => {
-    let val = {
-        select_1: $('#select_1 option:selected').text(),
-        select_2: $('#select_2 option:selected').text(),
-        select_3: $('#select_3 option:selected').text(),
-        select_4: $('#select_4 option:selected').text(),
-        select_5: $('#select_5 option:selected').text(),
-        textarea_1: $('#textarea_1').val(),
-        textarea_2: $('#textarea_2').val(),
-    }
-    $('#form_value').replaceWith('<div class="form__value" id="form_value">'.concat(JSON.stringify(val)),'</div>')
+// Получение данных 
 
+function getData() {
+    $('.form').find('select, textarea').each(function() {
+        val[this.id] = this.value;
+        str = JSON.stringify(val);
+        $('.form__value').text(str);
+    });
+};
+
+// Отправка данных
+
+function sendData() {
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://gimatutdinova.github.io/form/', true);
-    xhr.send(null);
-});
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
+    xhr.send(str);
+    xhr.onload = function() {
         if (xhr.status === 200) {
             alert("Данные дошли")
         }
-    }
-}
+    };
+};
 
+$('#button').on('click', () => {
+    getData();
+    sendData();    
+});
